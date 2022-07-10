c8ecf61eb7e26f99355a4e30b36514ab88e73981
Deepakraj15 <deepakraj15112002@gmail.com>
2022-07-10 22:04:01 +0530

    Box shadow added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,8 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './Cards.css';
 function Cards() {
     let colors = [];
-    let isCopied = false;
+    let copied, setColor;
+
     const hexaDecimals = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let colorString = '#';
     for (let i = 0; i < 5; i++) {
@@ -21,13 +22,19 @@ function Cards() {
             <div className='cards'>
                 {colors.map((color, index) => (
 
-                    <div className="card" key={index} style={{ backgroundColor: color }}>
-                        <CopyToClipboard onCopy={isCopied = true} text={color}>
+                    <div className="card" key={index} style={{ backgroundColor: color, boxShadow: `10px 5px 5px ${color} ` }}>
+                        <CopyToClipboard onCopy={() => { copied = true; setColor = color; }} text={color}>
                             {color == '#FFFFFF' ? <div className='text' style={{ color: 'black' }}>{color}</div> : <div className='text' style={{ color: 'white' }}>{color}</div>}
                         </CopyToClipboard>
                     </div>
-                ))}
+
+                ))
+                }
+
             </div>
+            {/* copied message */}
+            {copied ? <div className='copy-text'>{setColor} Copied!!</div> : null}
+
             <div>
                 <Button />
             </div>
