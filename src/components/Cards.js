68d04d7b5af231e6b0747b87cc0386f59e595b78
Deepakraj15 <deepakraj15112002@gmail.com>
2022-07-11 08:56:07 +0530

    alert message updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
 import Button from './Button';
+import { AlertContainer, alert } from 'react-custom-alert';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import copyicon from './copyicon.png';
 import './Cards.css';
 function Cards() {
     let colors = [];
-    const [isCopied, setCopy] = useState(false);
+    let isCopied = false;
     const hexaDecimals = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
     let colorString = '#';
     for (let i = 0; i < 5; i++) {
@@ -16,36 +16,27 @@ function Cards() {
         colors[i] = colorString;
         colorString = '#';
 
-    } useEffect(
-        () => {
-            let timer1 = setTimeout(() => setCopy(false), 5000);
-            return () => {
-                clearTimeout(timer1);
-            };
-        },
-        [isCopied]);
+    }
+    const alertSuccess = () => alert({ message: 'Copied to clipboard', type: 'success' });
 
     return <>
         <div className="container">
             <div className='cards'>
                 {colors.map((color, index) => (
                     <div className="card" key={index} style={{ backgroundColor: color, boxShadow: `7px 5px 5px ${color} ` }}>
-                        <CopyToClipboard onCopy={() => setCopy(true)} text={color}>
+                        <CopyToClipboard onCopy={() => { alertSuccess() }} text={color}>
                             {color === '#FFFFFF' ? <div className='text' style={{ color: 'black' }}>{color} <span className='icon-holder'>
                                 <img className='copy-icon' src={copyicon} alt='image not found' />
                             </span></div> : <div className='text' style={{ color: 'white' }}>{color} <span className='icon-holder'>
                                 <img className='copy-icon' src={copyicon} alt='image not found' />
                             </span></div>}
                         </CopyToClipboard>
-
                     </div>
-
-
                 ))
                 }
             </div>
-            {/* copied message */}
-            {isCopied ? <div className='copy-text'>Copied to clipboard!!</div> : null}
+
+            <AlertContainer floatingTime={3000} />
 
             <div>
                 <Button />
